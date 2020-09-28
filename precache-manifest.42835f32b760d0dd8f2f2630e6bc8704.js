e5c4dcc7aa2d226bc24eaa504d41c42546878904
Orlando Carnate <orlando.carnate@gmail.com>
2020-09-28 17:11:38 -0500

    Updates

---
+++ precache-manifest.42835f32b760d0dd8f2f2630e6bc8704.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4578d69dea888b9d83e",
+    "url": "/react-memory-game/static/css/main.857ec309.chunk.css"
+  },
+  {
+    "revision": "e4578d69dea888b9d83e",
+    "url": "/react-memory-game/static/js/main.84974c66.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0557c67bbb85b7a112e82d6be747df5a",
+    "url": "/react-memory-game/index.html"
+  }
+];
